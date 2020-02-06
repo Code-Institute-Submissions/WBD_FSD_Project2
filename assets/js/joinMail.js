dd04f98d92f7a90887af91de49451f92dbaf9a77
Bill Davis <bill.davis@gmail.com>
2020-02-06 05:57:39 +0000

    Added Clear Form button to Join form, made form clear itself after successful send of email, and display alert that form info was sent.

--- assets/js/joinMail.js
+++ assets/js/joinMail.js
@@ -14,10 +14,17 @@ function sendJoinMail(contactForm) {
     .then(
         function(response) {
             console.log("SUCCESS", response);
+            //Clear form on success
+            //document. getElementById("joinform"). reset();
+            // document.contactForm.reset();
+            contactForm.reset();
+            alert("Your membership request has been sent!");
         },
         function(error) {
             console.log("FAILED", error);
+            alert("Your membership request could not be sent at this time; please try again later.");
         }
     );
+
     return false;  // To block from loading a new page
 }
